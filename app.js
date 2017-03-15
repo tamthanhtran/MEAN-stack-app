ad891bf8ec8e6687091db5d14da224ffcecb6b11
TCMT\tamtt_000 <thanhtam0912@gmail.com>
2017-03-15 09:53:41 -0400

    Added index.html

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-require("./instantHello");
-var goodbye = require("./talk/goodbye");
-var talk = require("./talk");
-var question = require("./talk/question");
+var express = require("express");
+var app = express();
+var path = require("path");
 
-talk.intro();
-talk.hello("Simon");
+app.set('port',3000);
 
-var answer = question.ask("How to get a job?");
-console.log(answer);
+app.use(express.static(path.join(__dirname,'public') ));
 
-goodbye();
+/*app.get('/', function(req, res){
+	console.log('GET the homepage');
+	res
+	.status(200)
+	.sendfile(path.join(__dirname,'public' ,'index.html') );
+}); */
+
+app.get('/json', function(req, res){
+	console.log('GET the json');
+	res
+	.status(200)
+	.json( {'jsonDara' : true});
+}); 
+
+app.get('/file', function(req, res){
+	console.log('GET the file');
+	res
+	.status(200)
+	.sendfile(path.join(__dirname, 'app.js') );
+}); 
+
+var server = app.listen(app.get('port'), function(){
+	var port = server.address().port;
+	console.log('Magic happens on port ' + port);
+});
