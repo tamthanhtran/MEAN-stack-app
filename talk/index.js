51d222261e004a76307a56b37f77b5ff68891e7d
TCMT\tamtt_000 <thanhtam0912@gmail.com>
2017-03-15 04:10:23 -0400

    test

---
+++ talk/index.js
@@ -0,0 +1,16 @@
+var filename = "tttindex.js";
+
+var hello = function(name)
+{	
+	console.log("Hello " + name);
+};
+
+var intro = function()
+{	
+	console.log("I'm a node file called " + filename);
+};
+
+module.exports = {
+	hello : hello,
+	intro : intro
+};
